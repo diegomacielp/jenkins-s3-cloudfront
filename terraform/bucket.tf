1b158660b90c2bd6c9a710af1de0d49481ee3a6a
diegomacieltallos <diego.maciel@tallos.com.br>
2022-11-26 19:58:19 -0300

    Terraform

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,15 +1,15 @@
-resource "aws_s3_bucket" "bucket_web" {
+resource "aws_s3_bucket" "bucket_site" {
   bucket = "${var.bucket["name"]}-${random_pet.bucket.id}"
   tags   = local.common_tags
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl_web" {
-  bucket = aws_s3_bucket.bucket_web.id
+resource "aws_s3_bucket_acl" "bucket_site_acl" {
+  bucket = aws_s3_bucket.bucket_site.id
   acl    = var.bucket["acl"]
 }
 
-resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.bucket_web.bucket
+resource "aws_s3_bucket_website_configuration" "bucket_site_configuration" {
+  bucket = aws_s3_bucket.bucket_site.bucket
 
   index_document {
     suffix = "index.html"
